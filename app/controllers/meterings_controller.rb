a70634988b0aab7df1ebb71c2bc9d6d4a444edd9
Stepanov Viacheslav <stepanov.vm@gmail.com>
2017-01-09 19:21:06 +0300

    user auth

--- app/controllers/meterings_controller.rb
+++ app/controllers/meterings_controller.rb
@@ -1,5 +1,5 @@
 class MeteringsController < ApplicationController
-
+    before_filter :authenticate_user!
     before_action :set_item, only: [:show, :edit, :update, :destroy, :tariffs]
 
     def index
--- app/controllers/tariffs_controller.rb
+++ app/controllers/tariffs_controller.rb
@@ -1,5 +1,6 @@
 class TariffsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!
 
   def index
     @items = Tariff.all
